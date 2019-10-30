196ed9be16a96bd5b80ccba3be047acfafab9d43
Ilya Mukhortov <>
2019-10-30 10:51:48 +1000

    pass

--- src/UploadButton.js
+++ src/UploadButton.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { Button, Intent, Position, Toaster } from '@blueprintjs/core';
+import { Button, Intent, Position, Toaster, Spinner } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 
 const StyledButton = styled(Button)`
   position: relative;
   overflow: hidden;
-  
+  cursor: pointer;
+ 
   input {
     position: absolute;
     left: 0;
@@ -19,10 +20,14 @@ const StyledButton = styled(Button)`
     opacity: 0;
     cursor: pointer;
   }
+  
+  input::-webkit-file-upload-button {
+    cursor:pointer;
+  }
 `;
 
 const UploadToaster = Toaster.create({
-    position: Position.TOP,
+    position: Position.TOP
 });
 
 export class UploadButton extends Component {
@@ -41,50 +46,84 @@ export class UploadButton extends Component {
     super(props);
 
     this.state = {
-      uploading: false
+      uploading: false,
+      filesTotal: 0,
+      filesUploaded: 0
     }
   }
 
-  uploadFile = (e) => {
+  setUploading = (value) => {
+    this.setState({
+      uploading: value
+    });
+  };
+
+  uploadFiles = async (e) => {
     const { url, data, fileFieldName, onUploaded } = this.props;
     const files = e.target.files;
-
     const config = {
       headers: {'Content-Type': 'multipart/form-data'},
     };
-    Array.from(files).forEach(file => {
+    this.setUploading(true);
+    this.setState({
+      filesTotal: files.length,
+      filesUploaded: 0
+    });
+
+    for (const file of files) {
       let formData = new FormData();
       formData.append(fileFieldName, file);
       Object.keys(data).map(key => {
         formData.append(key, data[key])
       });
 
-      axios.post(url, formData, config).then(res => {
-        onUploaded(res.data);
-      })
-      .catch(error => {
-        const response = error.response;
-        if (response && response.status === 400) {
-          if (response.data && response.data.file) {
-            UploadToaster.show({
-              intent: Intent.DANGER,
-              message: `${file.name} - ${response.data.file[0]}`
-            });
+      const upload = async() => {
+        return axios.post(url, formData, config).then(res => {
+          onUploaded(res.data);
+        })
+        .catch(error => {
+          const response = error.response;
+          if (response && response.status === 400) {
+            if (response.data && response.data.file) {
+              UploadToaster.show({
+                intent: Intent.DANGER,
+                message: `${file.name} - ${response.data.file[0]}`
+              });
+            }
           }
-        }
-      });
-    });
+        })
+        .then(() => {
+          this.setState({
+            filesUploaded: this.state.filesUploaded + 1
+          });
+        });
+      };
+
+      await upload();
+    }
+
+    this.setUploading(false);
   };
 
   render() {
     const { buttonProps } = this.props;
+    const { uploading, filesTotal, filesUploaded } = this.state;
     return (
       <StyledButton
-        icon={IconNames.UPLOAD}
-        text={'Загрузить'}
+        icon={uploading ? <Spinner size={16}/> : IconNames.UPLOAD}
+        disabled={uploading}
+        active={uploading}
         {...buttonProps}
       >
-        <input type="file" id="imageButton" multiple onChange={(e) => this.uploadFile(e)} />
+        {uploading && (
+          <React.Fragment>{filesUploaded} из {filesTotal}</React.Fragment>
+        )}
+        {!uploading && (
+          <React.Fragment>
+            Загрузить
+            <input type="file" id="imageButton" multiple onChange={(e) => this.uploadFiles(e)} />
+          </React.Fragment>
+        )}
       </StyledButton>
     )
   }
